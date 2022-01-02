3ae53882033c790d76b122f0b94b4b843c0b4293
Marcelo213 <95463149+Marcelo213@users.noreply.github.com>
2022-01-02 05:44:17 -0500

    Update Serial.h

--- Serial.h
+++ Serial.h
@@ -1,24 +1,33 @@
-#ifndef SERIAL_H // Note: Is this way of naming identifiers in all caps a convention? Ask Taha
-#define SERIAL_H
+#pragma once
 
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/asio/serial_port.hpp>
 #include <string>
-#include <stdlib.h>
-#include <cstdlib>
-
 
 class Serial{
     private:
-        boost::asio::io_service io; 
+        double something;
+        std::string portName = "/dev/ttyUSB0";
+        static boost::asio::io_service io;
+        static boost::asio::serial_port port(boost::asio::io_service io, std::string portName = "/dev/ttyUSB0");
+        //port = boost::asio::serial(ioservice, "/dev/ttyACM0").new();
 
     public:
-        // Constructor for Serial Port
-        Serial(std::string portName);
-    
+        Serial();
+        ~Serial();
+
+        // Setting Functions
+        void s_default();
+        bool s_portName(std::string port);
+        bool s_baudRate(int rate);
+        bool s_parity(int parity);
+        bool s_stopBit(int bits);
+        bool s_charSize(int size);
+
         // Doing Functions
         void sendString(std::string sendData);
         
+        // Getting Functions
+        std::string g_data();
 };
-
-#endif
