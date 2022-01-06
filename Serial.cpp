6a1fa7541a36c2214cff1ec9680b6facf69c1c47
Marcelo213 <95463149+Marcelo213@users.noreply.github.com>
2022-01-06 07:34:38 -0500

    Update Serial.cpp

--- Serial.cpp
+++ Serial.cpp
@@ -1,14 +1,43 @@
 #include "Serial.h" // The header file we are declaring 
 
-Serial::Serial(){}
-Serial::~Serial(){}
+Serial::Serial(){
+  // From Herk.cpp ; Constructor acts as default
+  _serial.reset(new boost::asio::serial_port(io));
+  std::string portName = "/dev/ttyUSB1";
+  _serial->open(portName);
+  _serial->set_option(boost::asio::serial_port_base::baud_rate(9600));
+}
 
-boost::asio::io_service io;
-boost::asio::serial_port port(boost::asio::io_service io, std::string portName = "/dev/ttyUSB0");
+Serial::~Serial(){
+  _serial->close();
+}
+
+// Setting Functions
+/*void Serial::s_default(){};
+
+bool Serial::s_portName(std::string port){
+  _serial->open(port);
+  return true;
+}
+
+bool Serial::s_baudRate(int rate){
+  _serial->set_option(boost::asio::serial_port_base::baud_rate(rate));
+  return true;
+}*/
+
+/*bool Serial::s_parity(int parity){}
+bool Serial::s_stopBit(int bits){}
+bool Serial::s_charSize(int size){}*/
 
 
+// Doing Functions
+// - Need to give arduino time to load up
 void Serial::sendString(std::string sendData){
-  port.write_some(boost::asio::buffer(sendData.c_str(), sendData.size()) )
+  boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
+  t.wait();
+  _serial->write_some(boost::asio::buffer(sendData.c_str(), sendData.size()));
 }
 
 
+//  Getting Functions
+//std::string Serial::g_data(){}
