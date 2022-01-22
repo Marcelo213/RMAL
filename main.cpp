ec45812295fc7511700eaf6f5dc957cee1d9f094
Marcelo213 <95463149+Marcelo213@users.noreply.github.com>
2022-01-22 10:14:18 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "Serial.h"
+
+int main(void){
+
+    std::string port = "/dev/ttyACM0";
+    Serial arduino(port);
+    
+    std::string command = "Turn motor CW.";
+    arduino.sendString(command);
+
+
+    std::cout << "Task complete." << std::endl;
+
+    return 0;
+}
