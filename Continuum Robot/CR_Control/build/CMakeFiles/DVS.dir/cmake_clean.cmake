b1950997aa4ecbed14ec87fd894668bd77b565da
Marcelo_Ubuntu <marcelotanglao3@gmail.com>
2022-07-04 23:17:10 -0400

    attempting to establish system in ubuntu

---
+++ Continuum Robot/CR_Control/build/CMakeFiles/DVS.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DVS.dir/main.cpp.o"
+  "CMakeFiles/DVS.dir/src/controller.cpp.o"
+  "CMakeFiles/DVS.dir/src/ctcr_model.cpp.o"
+  "CMakeFiles/DVS.dir/src/group_bulk_read.cpp.o"
+  "CMakeFiles/DVS.dir/src/group_bulk_write.cpp.o"
+  "CMakeFiles/DVS.dir/src/group_sync_read.cpp.o"
+  "CMakeFiles/DVS.dir/src/group_sync_write.cpp.o"
+  "CMakeFiles/DVS.dir/src/mainloop.cpp.o"
+  "CMakeFiles/DVS.dir/src/packet_handler.cpp.o"
+  "CMakeFiles/DVS.dir/src/port_handler.cpp.o"
+  "CMakeFiles/DVS.dir/src/port_handler_arduino.cpp.o"
+  "CMakeFiles/DVS.dir/src/port_handler_linux.cpp.o"
+  "CMakeFiles/DVS.dir/src/port_handler_mac.cpp.o"
+  "CMakeFiles/DVS.dir/src/port_handler_windows.cpp.o"
+  "CMakeFiles/DVS.dir/src/protocol1_packet_handler.cpp.o"
+  "CMakeFiles/DVS.dir/src/protocol2_packet_handler.cpp.o"
+  "CMakeFiles/DVS.dir/src/robot_independent.cpp.o"
+  "CMakeFiles/DVS.dir/src/tdcr_model.cpp.o"
+  "CMakeFiles/DVS.dir/src/tdcr_model_DVS.cpp.o"
+  "CMakeFiles/DVS.dir/src/visualizer.cpp.o"
+  "DVS"
+  "DVS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DVS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
