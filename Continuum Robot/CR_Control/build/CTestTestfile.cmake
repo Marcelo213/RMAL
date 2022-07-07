afc8cc2941af222e4a2392342228d66b85297a2a
Marcelo_Ubuntu <marcelotanglao3@gmail.com>
2022-07-07 12:58:14 -0400

    ooo

--- Continuum Robot/CR_Control/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marcelo/Desktop/RMAL/Continuum Robot/CR_Control
-# Build directory: /home/marcelo/Desktop/RMAL/Continuum Robot/CR_Control/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
