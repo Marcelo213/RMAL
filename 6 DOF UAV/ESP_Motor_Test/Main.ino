3c68a93be71b831c51b82db52c753f13a4e8ab6a
Marcelo Tanglao <95463149+MarceloTanglao@users.noreply.github.com>
2022-09-24 18:33:00 -0400

    Implementing MPU9250

--- 6 DOF UAV/ESP_Motor_Test/Main.ino
+++ 6 DOF UAV/ESP_Motor_Test/Main.ino
@@ -1,57 +1,85 @@
-/*// the number of the LED pin
-const int ledPin = 26;  // 16 corresponds to GPIO16
+/*
+* Brian R Taylor
+* brian.taylor@bolderflight.com
+* 
+* Copyright (c) 2021 Bolder Flight Systems Inc
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the “Software”), to
+* deal in the Software without restriction, including without limitation the
+* rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+* sell copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+* IN THE SOFTWARE.
+*/
 
-// setting PWM properties
-const int freq = 50;
-const int ledChannel = 0;
-const int resolution = 12;
- 
-//////////////
-#include <Servo.h>
+#include "mpu9250.h"
 
-Servo ESC;     // create servo object to control the ESC
+int I2C_SDA = 27;
+int I2C_SCL = 32;
 
-int potValue;  // value from the analog pin
 
+/* Mpu9250 object */
+bfs::Mpu9250 imu;
 
 void setup() {
-  // initialize serial communication at 115200 bits per second:
+  /* Serial to display data */
   Serial.begin(115200);
-  
-  //set the resolution to 12 bits (0-4096)
-  analogReadResolution(12);
+  while(!Serial) {}
+  /* Start the I2C bus */
+  Wire.begin(I2C_SDA, I2C_SCL);
 
-
-////////////
-  // configure LED PWM functionalitites
-  //ledcSetup(ledChannel, freq, resolution);
-  
-  // attach the channel to the GPIO to be controlled
-  //ledcAttachPin(ledPin, ledChannel);
-  //////////////////
-    // Attach the ESC on pin 9
-  ESC.attach(26,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+  Wire.setClock(400000);
+  /* I2C bus,  0x68 address */
+  imu.Config(&Wire, bfs::Mpu9250::I2C_ADDR_PRIM);
+  /* Initialize and configure IMU */
+  if (!imu.Begin()) {
+    Serial.println("Error initializing communication with IMU");
+    while(1) {}
+  }
+  /* Set the sample rate divider */
+  if (!imu.ConfigSrd(19)) {
+    Serial.println("Error configured SRD");
+    while(1) {}
+  }
 }
 
 void loop() {
-  // read the analog / millivolts value for pin 2:
-  int analogValue = analogRead(34);
-  
-  // print out the values you read:
-  Serial.printf("ADC analog value = %d\n",analogValue);
-  
-  delay(100);  // delay in between reads for clear read from serial
-
-
-////////////
-
-
-  //potValue = analogRead(A0);   // reads the value of the potentiometer (value between 0 and 1023)
-  potValue = map(analogValue, 0, 4095, 0, 180);   // scale it to use it with the servo library (value between 0 and 180)
-  Serial.printf("The PWM value = %d\n",potValue);
-  ledcWrite(ledChannel, potValue); 
-  //ESC.write(potValue);    // Send the signal to the ESC
-
-
-  
-}*/
+  /* Check if data read */
+  if (imu.Read()) {
+    Serial.print(imu.new_imu_data());
+    Serial.print("\t");
+    Serial.print(imu.new_mag_data());
+    Serial.print("\t");
+    Serial.print(imu.accel_x_mps2());
+    Serial.print("\t");
+    Serial.print(imu.accel_y_mps2());
+    Serial.print("\t");
+    Serial.print(imu.accel_z_mps2());
+    Serial.print("\t");
+    Serial.print(imu.gyro_x_radps());
+    Serial.print("\t");
+    Serial.print(imu.gyro_y_radps());
+    Serial.print("\t");
+    Serial.print(imu.gyro_z_radps());
+    Serial.print("\t");
+    Serial.print(imu.mag_x_ut());
+    Serial.print("\t");
+    Serial.print(imu.mag_y_ut());
+    Serial.print("\t");
+    Serial.print(imu.mag_z_ut());
+    Serial.print("\t");
+    Serial.print(imu.die_temp_c());
+    Serial.print("\n");
+  }
+}
