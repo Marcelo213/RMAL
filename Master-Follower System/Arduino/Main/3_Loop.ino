41f75fb7cbb195bf71dd44d78319f303285ed230
Marcelo Tanglao <95463149+MarceloTanglao@users.noreply.github.com>
2022-11-08 21:14:56 -0500

    update

--- Master-Follower System/Arduino/Main/3_Loop.ino
+++ Master-Follower System/Arduino/Main/3_Loop.ino
@@ -3,10 +3,11 @@ void loop(){
   /*if (Serial.available()) {      // If anything comes in Serial (USB),
     Serial.write(Serial.read());   // read it and send it out Serial1 (pins 0 & 1)
   }*/
-  if ((millis() - loop1Time100Hz) > 10) {
+  /*if ((millis() - loop1Time100Hz) > 10) {
     loop1_100Hz();
     loop1Time100Hz = millis(); //Update timer
-  }
+  }*/
+  read_encoder();
   if (Serial.available() > 0){
     //sSerial.flush();
     char command = Serial.read();
@@ -16,11 +17,13 @@ void loop(){
       Serial.print("HERE");
       //Serial.print("The command was: ");
       //Serial.println(command);
-      CATHETER_ROTATING_CCW(50);
+      ASSIST_LINEAR_FORWARD(100);
     }
   }
 }
 
-void loop1_100Hz() {
+
+
+/*void loop1_100Hz() {
   read_encoder();
-}
+}*/
