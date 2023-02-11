2b3d4f12a689ab7859c1c705c9a0e16cfb2413b1
Marcelo Tanglao <marcelotanglao3@gmail.com>
2023-02-11 18:28:37 -0500

    Reorganizing

---
+++ uam/ros_uam/build/aruco_detector/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/marcelo/Documents/GitHub/RMAL/uam/ros_uam/install/aruco_detector'
---
+++ uam/ros_uam/install/aruco_detector/lib/python3.8/site-packages/aruco_detector/detector.py
@@ -0,0 +1,110 @@
+import rclpy                              # This library stands for ROS2 Client Library Python. It allows Python to access the ROS2 infrastructure
+from rclpy.node import Node               # This line allows us to use the Node class fromt he rclpy library
+from sensor_msgs.msg import Image         # Image is the message type; sensors_msgs is the message library 
+
+
+
+# import the necessary packages     
+from imutils.video import VideoStream     # Accesses webcam
+import time                               # Gives access to delay functions
+import argparse                           # For Command Line Commands
+import imutils                            # For resizing images
+import cv2                                # Access to opencv
+import sys                                # For system control (exiting)
+
+
+
+
+# define names of each possible ArUco tag OpenCV supports
+ARUCO_DICT = {
+	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}                   
+
+# load the ArUCo dictionary and grab the ArUCo parameters
+print("[INFO] detecting '{}' tags...".format(ARUCO_DICT["DICT_ARUCO_ORIGINAL"]))
+arucoDict = cv2.aruco.getPredefinedDictionary(ARUCO_DICT["DICT_ARUCO_ORIGINAL"])
+arucoParams = cv2.aruco.DetectorParameters()
+detector = cv2.aruco.ArucoDetector(arucoDict, arucoParams)
+# initialize the video stream and allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+# loop over the frames from the video stream
+while True:
+
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 1000 pixels
+	frame = vs.read()
+	frame = imutils.resize(frame, width=1000)
+
+	# detect ArUco markers in the input frame
+	(corners, ids, rejected) = detector.detectMarkers(frame)
+	
+	# verify *at least* one ArUco marker was detected
+	if len(corners) > 0:
+		# flatten the ArUco IDs list
+		ids = ids.flatten()
+		# loop over the detected ArUCo corners
+		for (markerCorner, markerID) in zip(corners, ids):
+			# extract the marker corners (which are always returned
+			# in top-left, top-right, bottom-right, and bottom-left
+			# order)
+			corners = markerCorner.reshape((4, 2))
+			(topLeft, topRight, bottomRight, bottomLeft) = corners
+			
+			# convert each of the (x, y)-coordinate pairs to integers
+			topRight = (int(topRight[0]), int(topRight[1]))
+			bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+			bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+			topLeft = (int(topLeft[0]), int(topLeft[1]))
+			
+			# draw the bounding box of the ArUCo detection
+			cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
+			cv2.line(frame, topRight, bottomRight, (0, 255, 0), 2)
+			cv2.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
+			cv2.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
+			
+			# compute and draw the center (x, y)-coordinates of the
+			# ArUco marker
+			cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+			cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+			cv2.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
+			
+			# draw the ArUco marker ID on the frame
+			cv2.putText(frame, str(markerID),
+				(topLeft[0], topLeft[1] - 15),
+				cv2.FONT_HERSHEY_SIMPLEX,
+				0.5, (0, 255, 0), 2)
+
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
