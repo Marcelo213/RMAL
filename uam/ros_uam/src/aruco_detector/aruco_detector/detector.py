b9b23fd54df275fb50bb462835f867fb554f1a73
Marcelo Tanglao <marcelotanglao3@gmail.com>
2023-02-11 18:25:44 -0500

    Reogranizing

--- uam/ros_uam/src/aruco_detector/aruco_detector/detector.py
+++ uam/ros_uam/src/aruco_detector/aruco_detector/detector.py
@@ -52,12 +52,15 @@ time.sleep(2.0)
 
 # loop over the frames from the video stream
 while True:
+
 	# grab the frame from the threaded video stream and resize it
 	# to have a maximum width of 1000 pixels
 	frame = vs.read()
 	frame = imutils.resize(frame, width=1000)
+
 	# detect ArUco markers in the input frame
 	(corners, ids, rejected) = detector.detectMarkers(frame)
+	
 	# verify *at least* one ArUco marker was detected
 	if len(corners) > 0:
 		# flatten the ArUco IDs list
@@ -69,32 +72,39 @@ while True:
 			# order)
 			corners = markerCorner.reshape((4, 2))
 			(topLeft, topRight, bottomRight, bottomLeft) = corners
+			
 			# convert each of the (x, y)-coordinate pairs to integers
 			topRight = (int(topRight[0]), int(topRight[1]))
 			bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
 			bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
 			topLeft = (int(topLeft[0]), int(topLeft[1]))
+			
 			# draw the bounding box of the ArUCo detection
 			cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
 			cv2.line(frame, topRight, bottomRight, (0, 255, 0), 2)
 			cv2.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
 			cv2.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
+			
 			# compute and draw the center (x, y)-coordinates of the
 			# ArUco marker
 			cX = int((topLeft[0] + bottomRight[0]) / 2.0)
 			cY = int((topLeft[1] + bottomRight[1]) / 2.0)
 			cv2.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
+			
 			# draw the ArUco marker ID on the frame
 			cv2.putText(frame, str(markerID),
 				(topLeft[0], topLeft[1] - 15),
 				cv2.FONT_HERSHEY_SIMPLEX,
 				0.5, (0, 255, 0), 2)
+
 	# show the output frame
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
+
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
+
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
